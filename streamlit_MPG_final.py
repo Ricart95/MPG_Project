3c9687e216330053c7fb7379831c5927845516fb
Ricart95 <143813994+Ricart95@users.noreply.github.com>
2024-03-20 16:30:01 +0100

    Update streamlit_MPG_final.py

--- streamlit_MPG_final.py
+++ streamlit_MPG_final.py
@@ -21,13 +21,13 @@ from sklearn.metrics import confusion_matrix
 from sklearn import tree
 from sklearn.metrics import classification_report
 from sklearn.ensemble import RandomForestClassifier
-from imblearn.over_sampling import RandomOverSampler
+
 from sklearn.model_selection import GridSearchCV
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 import statistics
-import statsmodels.api as sm
+
 import joblib
 from sklearn.metrics import r2_score
 from sklearn.ensemble import GradientBoostingClassifier
